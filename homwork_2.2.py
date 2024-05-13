5f3de999b1d96cc45fd298ff9c2961ce7d1b2a93
Vladimirrrrr1 <168712085+Vladimirrrrr1@users.noreply.github.com>
2024-05-13 16:18:24 +0300

    Add files via upload

---
+++ homwork_2.2.py
@@ -0,0 +1,17 @@
+a = 15
+b = 10
+if a > b:
+    print('Ok')
+if (b < a) and (a > b or b < 10):
+    print('Ok')
+if b > 5 and a > 8:
+    print('Ok')
+a1 = '35'
+a2 = '211'
+if a1 > a2:
+    print('Ok')
+    #нельзя сравнить разные типы
+   # if [a + b] > 15:
+    # if '35' == 35:
+
+
