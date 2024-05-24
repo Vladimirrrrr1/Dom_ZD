1c9d9a81cc58590dbbe0e98b5f30d7fa16d3adfd
Vladimirrrrr1 <168712085+Vladimirrrrr1@users.noreply.github.com>
2024-05-24 22:39:30 +0300

    Add files via upload

---
+++ DZ_function.py
@@ -0,0 +1,16 @@
+
+def print_params(name):
+    print('hello', name)
+    par1 = int(input('число1 :'))
+    par2 = int(input('число2 :'))
+    print((par1 + par2) * 2)
+
+
+
+
+
+
+
+print_params('vova')
+
+
