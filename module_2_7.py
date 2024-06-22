e92fdd0ef76e869c3a055eb0ed1dc5ce35310d9e
Vladimirrrrr1 <168712085+Vladimirrrrr1@users.noreply.github.com>
2024-06-22 22:13:05 +0300

    Add files via upload

--- module_2_7.py
+++ module_2_7.py
@@ -1,12 +1,14 @@
-def print_params(a=1, b='vova', c=True):
-    print(a, b, c)
-values_list = [1, 'vova', True]
-values_dict = {'a': 1, 'b': 'vova', 'c': True}
-values_list_2 = [12.5, 'vova']
-print_params()
-print_params(2, 'denis', False)
-print_params(b=25)
-print_params(c=[1,2,3])
-print_params(*values_list)
-print_params(**values_dict)
-print_params(*values_list_2, 42)
+
+def get_multiplied_digits(namber):
+    str_namber = str(namber)
+    first = int(str_namber[0])
+    if first == 0:
+        first = 1
+    if len(str_namber) == 1:
+        return first
+    else:
+        return first * get_multiplied_digits(int(str_namber[1:]))
+
+
+result = get_multiplied_digits(input('chislo :'))
+print(result)
